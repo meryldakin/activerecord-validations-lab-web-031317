3a90ddd197a114d4857a3187dd03e5d863750c74
meryldakin <meryl.k.dakin@gmail.com>
2017-04-12 10:55:17 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+  validates :title, presence: true
+  validates :content, length: { minimum: 15 }
+  validates :summary, length: { maximum: 30 }
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :title, format: { with: /\A(You Won't Believe)/ }
 end
